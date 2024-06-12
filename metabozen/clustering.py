Fixed duplicate column bug in clustering and empty qval calculation in stats_tests

metabozen/clustering.py
@@ -311,9 +311,9 @@ def prettify_output(args):
         df_out.insert(insert_index, f'{sg}_mean', df_out[sg_samples].fillna(0.0).mean(axis=1))
     rt_cols = ['rt_' + x for x in args.sample_names]
     mz_cols = ['mz_' + x for x in args.sample_names]
-    df_out_temp = pd.DataFrame(df_out[[x for x in df_out.columns if x not in (args.sample_names + rt_cols)]])
-    df_out = pd.concat([df_out_temp, pd.DataFrame(df_out[list(args.sample_names) + rt_cols])], axis=1, sort=False)
-    df_out = pd.concat([df_out[[x for x in df_out.columns if x not in mz_cols]], df_out[mz_cols]], axis=1)    
+    df_out_temp = pd.DataFrame(df_out[[x for x in df_out.columns if x not in (list(args.sample_names) + rt_cols + mz_cols)]])
+    df_out = pd.concat([df_out_temp, pd.DataFrame(df_out[list(args.sample_names) + rt_cols + mz_cols])], axis=1, sort=False)
+    # df_out = pd.concat([df_out[[x for x in df_out.columns if x not in mz_cols]], df_out[mz_cols]], axis=1)    
     return df_out
 
 ################################################################################

metabozen/stats_tests.py
@@ -113,9 +113,14 @@ def run_mann_whitney_u(args):
         if (df_out.iloc[i][sns_g1].isnull().all() & df_out.iloc[i][sns_g2].isnull().all()): 
             stat, pval = scipy.stats.mannwhitneyu([1e-11, 0, 0], [1e-11, 0], alternative = 'two-sided')
         elif not args.config['mann_whitney_u']['kwargs']['paired']:
-            stat, pval = scipy.stats.mannwhitneyu(df_out.iloc[i][sns_g1].fillna(value = 0.0), df_out.iloc[i][sns_g2].fillna(value = 0.0), alternative = 'two-sided')
+            stat, pval = scipy.stats.mannwhitneyu(
+                df_out.iloc[i][sns_g1].astype(float).fillna(value = 0.0), 
+                df_out.iloc[i][sns_g2].astype(float).fillna(value = 0.0), 
+                alternative = 'two-sided')
         else:
-            stat, pval = scipy.stats.wilcoxon(df_out.iloc[i][sns_g1].fillna(value = 0.0), df_out.iloc[i][sns_g2].fillna(value = 0.0), mode = 'exact')
+            stat, pval = scipy.stats.wilcoxon(df_out.iloc[i][sns_g1].astype(float).fillna(value = 0.0),
+                                              df_out.iloc[i][sns_g2].astype(float).fillna(value = 0.0),
+                                              mode = 'exact')
         stats[i] = stat
         pvals[i] = pval
     out_cols = ['mann_whitney_u_muStats', 'mann_whitney_u_muPvals']
@@ -126,7 +131,7 @@ def run_mann_whitney_u(args):
     if args.config['mann_whitney_u']['kwargs']['qvalues']:
         out_cols.append('mann_whitney_u_muQvals')
         # plot_cols['mann_whitney_u_muQvals'] = "Q-values Mann-Whitney parent features"
-        df_parents = df_out[df_out.apply(lambda x: x.name == x['parent_peak'], axis=1)]
+        df_parents = df_out[df_out.apply(lambda x: x['name'] == x['parent_peak'], axis=1)]
         try:
             qresult = qvalue.qvalue(p = ro.FloatVector(df_parents['mann_whitney_u_muPvals'].values))
             df_qresults = pd.DataFrame(np.array(qresult.rx('qvalues')[0]), index = df_parents.index)